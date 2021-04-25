d94a6e85328baa3b40bc7bd5e063ba7f9be450be
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-25 12:52:10 +0200

    fixed npm ERR! missing script: start

--- node-red-service1/entrypoint.sh
+++ node-red-service1/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Copying settings.js to /data folder"
 cp settings.js /data/settings.js
-cp package.json /data/package.json
+#cp package.json /data/package.json
 
 echo "Starting node-red ..."
 npm start -- --userDir /data

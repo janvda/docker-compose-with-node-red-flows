705a80827ccfcd71d67c87fe3ed2d97d4af8e973
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-25 12:36:36 +0200

    draft version

---
+++ node-red-service1/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Copying settings.js to /data folder"
+cp settings.js /data/settings.js
+cp package.json /data/package.json
+
+echo "Starting node-red ..."
+npm start -- --userDir /data

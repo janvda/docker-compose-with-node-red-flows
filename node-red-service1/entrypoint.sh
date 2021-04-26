826049326a535c7968a62899447f0db7b0297734
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-26 13:32:57 +0200

    cleanup of unused files

--- node-red-service1/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#echo "Copying settings.js to /usr/src/node-red/.node-red/settings.js"
-#cp /node-red/settings.js      ./settings.js
-#cp flows.json       /data/flows.json
-#cp flows_cred.json  /data/flows_cred.json
-
-echo "Starting node-red ..."
-npm start --  --userDir /node-red 

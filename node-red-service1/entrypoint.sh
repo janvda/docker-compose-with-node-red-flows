91c529745ca1b649179d6758e2b578525323f810
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-26 09:48:46 +0200

    fix attempt

--- node-red-service1/entrypoint.sh
+++ node-red-service1/entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "Copying settings.js to /data folder"
 cp settings.js /data/settings.js
-cp /tmp/package.json /data/package.json
 cp /tmp/flows.json   /data/flows.json
 
 echo "Starting node-red ..."

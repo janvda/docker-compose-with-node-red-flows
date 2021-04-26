18dbb41a4757718a85d033c376066164c23cc399
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-26 10:02:31 +0200

    set rootPath in settings.js

--- node-red-service1/settings.js
+++ node-red-service1/settings.js
@@ -49,11 +49,13 @@ module.exports = {
     debugMaxLength: 1000,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    flowFile: 'flows.json',
+    //flowFile: 'flows.json',
+
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
     flowFilePretty: true,
+    rootPath: 'node-red-service1/',
 
     // By default, all user data is stored in the Node-RED install directory. To
     // use a different location, the following property can be used

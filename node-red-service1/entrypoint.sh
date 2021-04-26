220593fa5275842a03b77d08debe4be4976c0dfb
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-04-26 11:08:34 +0200

    minimize copying

--- node-red-service1/entrypoint.sh
+++ node-red-service1/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "Copying settings.js to /data folder"
-cp settings.js      /data/settings.js
-cp flows.json       /data/flows.json
-cp flows_cred.json  /data/flows_cred.json
+#echo "Copying settings.js to /usr/src/node-red/.node-red/settings.js"
+#cp /node-red/settings.js      ./settings.js
+#cp flows.json       /data/flows.json
+#cp flows_cred.json  /data/flows_cred.json
 
 echo "Starting node-red ..."
-npm start -- --userDir /data 
-# --prefix ./node-red-service1
+npm start --  --userDir /node-red 

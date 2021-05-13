a11ad1e644cbaf274739b2634aa027d7aa660bc0
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-05-13 15:18:01 +0200

    typo

--- node-red-service1/settings.js
+++ node-red-service1/settings.js
@@ -185,7 +185,7 @@ module.exports = {
         },
         header: {
          title: "node-red-service 1"
-        },
+        }
         // jva 2021-04-26: project functionality is enabled through env variable: NODE_RED_ENABLE_PROJECTS
         //projects: {
         //    enabled: true
